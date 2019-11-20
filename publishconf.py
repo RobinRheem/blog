015bfbd6aac8bd580c2968b7af649f7bb9638cd4
RobinRheem <robin.rheem@gmail.com>
2019-11-20 09:12:12 +0900

    Remove feed related settings

--- publishconf.py
+++ publishconf.py
@@ -14,10 +14,10 @@ from pelicanconf import *
 SITEURL = 'https://robinrheem.github.io'
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+#FEED_ALL_ATOM = 'feeds/all.atom.xml'
+#CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
-DELETE_OUTPUT_DIRECTORY = True
+DELETE_OUTPUT_DIRECTORY = False
 
 # Following items are often useful when publishing
 
