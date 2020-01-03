8d82b3decab37c12712d0f29d458b533594172e9
RobinRheem <robin.rheem@gmail.com>
2020-01-03 21:59:14 +0900

    Remove site url config

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,6 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Robin Rheem'
 SITENAME = 'Robin Rheem'
-SITEURL = 'https://robinrheem.com'
 SITESUBTITLE = 'Software Engineer'
 
 THEME = 'pelican-clean-blog'
